5a7deb4c20a41a034865aa26fbed1dfccb6ac419
Alexandros Raikos <alexandros@araikos.gr>
2021-04-16 16:58:24 +0300

    added ignored filenames and extensions

---
+++ dextractor/exclusions.py
@@ -0,0 +1,58 @@
+# ---------
+# This source file is part of the Dependency Extractor python open source package.
+# Copyright (c) 2021, Alexandros Raikos tou Konstantinou.
+#
+# Licensed under the MIT License.
+# ---------
+#
+# Define a list of file extensions and names to be ignored.
+
+ignored_files = [
+    ".gitignore",
+    "README",
+    "LICENSE",
+    "MAINTAINERS",
+    "BUGS",
+    "CONTRIBUTING",
+    "CONTRIBUTORS",
+    "AUTHORS",
+    "PATENTS",
+    "Dockerfile",
+    "Makefile",
+]
+
+ignored_extensions = [
+    ".png",
+    ".ico",
+    ".jpg",
+    ".svg",
+    ".tiff",
+    ".yml",
+    ".yaml",
+    ".rst",
+    ".json",
+    ".xml",
+    ".html",
+    ".har",
+    ".properties",
+    ".plist",
+    ".all",
+    ".txt",
+    ".doc",
+    ".xls",
+    ".ppt",
+    ".docx",
+    ".xlsx",
+    ".pptx",
+    ".csv",
+    ".jmx",
+    ".cmd",
+    ".sh",
+    ".mod",
+    ".sum",
+    ".tpl",
+    ".npy",
+    ".npz",
+    ".ini",
+    ".inc"
+]
